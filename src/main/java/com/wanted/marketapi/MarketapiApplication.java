d884dd21487028a6c512d9b20cd3c7455d7f3586
papicc45 <papicc45@naver.com>
2024-06-01 15:22:17 +0900

    initial commit

---
+++ src/main/java/com/wanted/marketapi/MarketapiApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.marketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketapiApplication.class, args);
+    }
+
+}
